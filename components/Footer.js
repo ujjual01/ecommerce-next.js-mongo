d59b70fc416f95ffd1a69305db7eb10c2f0bec65
ujjual <ujjual72@gmail.com>
2022-01-28 18:02:09 +0530

    added products

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-  <div  className="flex justify-center items-center fixed inset-x-0 bottom-0">
+    
+  <div  className="flex justify-center items-center inset-x-0 bottom-0 mt-5 p-6 pt-4">
+    
       Made with {' '}
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
   <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
--- components/Products.js
+++ components/Products.js
@@ -1,13 +1,22 @@
+import { PrinterIcon } from '@heroicons/react/solid';
 import React from 'react';
+import data from '../utils/data';
 
 const Products = () => {
   return <div className="">
-    Products
-    <ul>
-        <li>product1</li>
-        <li>Produt2</li>
-        <li>product3</li>
-    </ul>
+    <h1 className="ml-2 text-3xl p-5 font-semibold hover:underline">Products</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mx-5">
+      {
+        data.products.map((product)=>(
+          <div className="p-2">
+            <img className="rounded-md object-cover" src={product.image} alt=""/>
+            <div className="text-2xl p-1">{product.name}</div>
+            <span className=" block">{`$ ${product.price}`}</span>
+            <button className="bg-secondary text-bc flex item-center px-5 py-2 rounded-md mt-3">Add to cart</button>
+          </div>
+        ))
+      }
+    </div>
   </div>;
 };
 
